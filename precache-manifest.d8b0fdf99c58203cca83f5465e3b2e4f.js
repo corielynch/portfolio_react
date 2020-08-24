be530a8638536ee47a39ea29024689f362d8877e
Corie Lynch <Lynchc17@gmail.com>
2020-08-24 14:16:06 -0700

    Updates

---
+++ precache-manifest.d8b0fdf99c58203cca83f5465e3b2e4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "650b95e1648a913c38465396b577986d",
+    "url": "/portfolio_react/index.html"
+  },
+  {
+    "revision": "9df6a8360ce6bd00310c",
+    "url": "/portfolio_react/static/css/main.c8360c6e.chunk.css"
+  },
+  {
+    "revision": "ea32e9dc8509da325faa",
+    "url": "/portfolio_react/static/js/2.bcfaa5ee.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio_react/static/js/2.bcfaa5ee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9df6a8360ce6bd00310c",
+    "url": "/portfolio_react/static/js/main.36469757.chunk.js"
+  },
+  {
+    "revision": "c5c18d2a1766a406992e",
+    "url": "/portfolio_react/static/js/runtime-main.7346848c.js"
+  }
+]);
